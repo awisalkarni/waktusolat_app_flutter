d0d44250e852a03f9c0cbcf791a80180260b4252
Awis Alkarni <awis@awislabs.com>
2020-04-20 19:19:03 +0800

    world clock

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,13 @@
-
-
-import 'quote.dart';
-import 'quote_card.dart';
 import 'package:flutter/material.dart';
+import 'package:waktusolatapp/pages/choose_location.dart';
+import 'package:waktusolatapp/pages/home.dart';
+import 'package:waktusolatapp/pages/loading.dart';
 
-void main() => runApp(
-    MaterialApp(
-        home: QuoteList(),
-    )
-);
-
-class QuoteList extends StatefulWidget {
-  @override
-  _QuoteListState createState() => _QuoteListState();
-}
-
-class _QuoteListState extends State<QuoteList> {
-
-  List<Quote> quotes = [
-    Quote(text: 'Be yourself; everyone is already taken', author: 'awis'),
-    Quote(text: 'I have nothing to declare except my genuies', author: 'awis'),
-    Quote(text: 'The trutch is rarely pure and never simple', author: 'awis')
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text("Waktu Solat Evo"),
-        centerTitle: true,
-        backgroundColor: Colors.redAccent,
-      ),
-      body: Column(
-        children: quotes.map((quote) => QuoteCard(
-            quote: quote,
-            delete: () {
-              setState(() {
-                quotes.remove(quote);
-              });
-            }
-        )).toList(),
-      ),
-    );
-  }
-}
-
-
+void main() => runApp(MaterialApp(
+  initialRoute: '/home',
+  routes: {
+    '/': (context) => Loading(),
+    '/home': (context) => Home(),
+    '/location': (context) => ChooseLocation(),
+  },
+));
